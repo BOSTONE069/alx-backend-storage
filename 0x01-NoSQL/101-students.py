3c6ab1ad05cea1dbc3dae3a4c1d9bef9b60fbe4c
BOSTONE069 <bostoneochieng69@gmail.com>
2023-02-14 10:52:53 -0500

    Python function that returns all students sorted by average score:

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,14 +5,19 @@
 def top_students(mongo_collection):
     # sourcery skip: inline-immediately-returned-variable
     """ Returns all students sorted by average score """
-    top_student = mongo_collection.aggregate([
+    pipeline = [
         {
             "$project": {
-                "name": "$name",
-                "averageScore": {"$avg": "$topics.score"}
+                "name": 1,
+                "scores": 1,
+                "averageScore": {"$avg": "$scores"}
             }
         },
-        {"$sort": {"averageScore": -1}}
-    ])
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+    cursor = mongo_collection.aggregate(pipeline)
+    students = list(cursor)
+    return students
 
-    return top_student
