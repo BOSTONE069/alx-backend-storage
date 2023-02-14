5ca8b4e8b7ff48830867b3808502275fa09b98f1
BOSTONE069 <bostoneochieng69@gmail.com>
2023-02-14 10:40:01 -0500

    Python function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ returns the list of school having a specific topic """
+    documents = mongo_collection.find({"topics": topic})
+    return list(documents)
